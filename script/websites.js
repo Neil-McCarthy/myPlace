509812557db05e06bc9ab1f24d07b183f66f26b9
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-10-26 13:43:09 +0100

    website page section to article change

--- script/websites.js
+++ script/websites.js
@@ -1,23 +1,23 @@
-let websiteSectionList = document.getElementById('websiteSection').getElementsByTagName('section');
+let websiteArticleList = document.getElementById('websiteSection').getElementsByTagName('article');
 let websiteChangeButtons = document.getElementsByTagName('main')[0].getElementsByTagName('button');
 let websiteBeingDisplayed = 0;
 for (let buttonPressed = 0;buttonPressed < websiteChangeButtons.length;buttonPressed++) {
     websiteChangeButtons[buttonPressed].onclick = () => {
         console.log(websiteBeingDisplayed, buttonPressed);
         if (buttonPressed == 0 && websiteBeingDisplayed != 0) {
-            websiteSectionList[websiteBeingDisplayed].style.marginLeft = '100%';
-            websiteSectionList[websiteBeingDisplayed - 1].style.width = '95vw';
+            websiteArticleList[websiteBeingDisplayed].style.marginLeft = '100%';
+            websiteArticleList[websiteBeingDisplayed - 1].style.width = '95vw';
             websiteBeingDisplayed--;
         }
-        else if (buttonPressed == 1 && websiteBeingDisplayed != websiteSectionList.length -1) {
-            websiteSectionList[websiteBeingDisplayed].style.width = 0;
-            websiteSectionList[websiteBeingDisplayed + 1].style.width = '95vw';
-            websiteSectionList[websiteBeingDisplayed + 1].style.marginLeft = 0;
+        else if (buttonPressed == 1 && websiteBeingDisplayed != websiteArticleList.length -1) {
+            websiteArticleList[websiteBeingDisplayed].style.width = 0;
+            websiteArticleList[websiteBeingDisplayed + 1].style.width = '95vw';
+            websiteArticleList[websiteBeingDisplayed + 1].style.marginLeft = 0;
             websiteBeingDisplayed++;
         }
         if (websiteBeingDisplayed == 0) {
             websiteChangeButtons[0].style.opacity = 0;
-        } else if (websiteBeingDisplayed == websiteSectionList.length -1) {
+        } else if (websiteBeingDisplayed == websiteArticleList.length -1) {
             websiteChangeButtons[1].style.opacity = 0;
         } else {
             websiteChangeButtons[0].style.opacity = 1;
