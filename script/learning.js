0f259562dac0b51716a120a78179eb70f88d4a97
Neil-McCarthy <73938609+Neil-McCarthy@users.noreply.github.com>
2022-11-07 15:44:32 +0000

    learning - polishing

--- script/learning.js
+++ script/learning.js
@@ -5,29 +5,29 @@ const learningData = [
     {
         name: "JavaScript",
         overview: "Overview of js",
-        implementation: "what you've done with js",
         level: "level of js",
+        implementation: "what you've done with js",
         futurePlans: "what you hope to do with js"
     },
     {
         name: "React",
         overview: "Overview of react",
-        implementation: "what you've done with react",
         level: "level of react",
+        implementation: "what you've done with react",
         futurePlans: "what you hope to do with react"
     },
     {
         name: "C#",
         overview: "Overview of C#",
-        implementation: "what you've done with C#",
         level: "level of js",
+        implementation: "what you've done with C#",
         futurePlans: "what you hope to do with C#"
     },
     {
         name: "MySQL",
         overview: "Overview of mySQL",
-        implementation: "what you've done with mySQL",
         level: "level of mySQL",
+        implementation: "what you've done with mySQL",
         futurePlans: "what you hope to do with mySQL"
     }
 ]
@@ -36,8 +36,8 @@ for (let singleIcon = 0;singleIcon < languagesIconList.length;singleIcon++) {
     languagesIconList[singleIcon].onclick = () => {
         learningDisplaySections[0].getElementsByTagName("h1")[0].innerHTML = learningData[singleIcon].name;
         learningDisplaySections[0].getElementsByTagName("p")[0].innerHTML = learningData[singleIcon].overview;
-        learningDisplaySections[1].getElementsByTagName("p")[0].innerHTML = learningData[singleIcon].implementation;
-        learningDisplaySections[2].getElementsByTagName("p")[0].innerHTML = learningData[singleIcon].level;
+        learningDisplaySections[1].getElementsByTagName("p")[0].innerHTML = learningData[singleIcon].level;
+        learningDisplaySections[2].getElementsByTagName("p")[0].innerHTML = learningData[singleIcon].implementation;
         learningDisplaySections[3].getElementsByTagName("p")[0].innerHTML = learningData[singleIcon].futurePlans;
     };
 }
